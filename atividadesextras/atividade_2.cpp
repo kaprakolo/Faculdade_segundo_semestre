f97dc1ba0bf721a5bc2fea9cee593632f1e974da
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-09-27 20:20:14 -0300

    Update atividade_2.cpp

--- atividadesextras/atividade_2.cpp
+++ atividadesextras/atividade_2.cpp
@@ -8,38 +8,44 @@ int main(){
 	
 	setlocale(LC_ALL, "portuguese");
 	
-	char nome[21], opcao;// armazena um nome com 21 caracteres
+	char nome[21], opcao, continuar;// armazena um nome com 21 caracteres
 	int resposta, goblin;
 	
+	 do {
 	
 	printf("digite seu nome: ");
-	scanf("%s", nome);// não vai e comercial pq é string
+	scanf("%s", nome);// nÃ£o vai e comercial pq Ã© string
 	
-	printf("Olá %s !", nome);
+	printf("OlÃ¡ %s !", nome);
 	 
 	printf("\nquer jogar? S/N");
 	scanf(" %c", &opcao);
 	
 	if(toupper(opcao)== 'N'){
-		printf("\nOK fica pra próxima!");
+		printf("\nOK fica pra prÃ³xima!");
 		return 0;
 	}
 	
 	srand(time(NULL));
 	goblin = 1 + rand() % 5; // numero aleatorio entre 0 ate 4
 	
-	printf("|_1_| |_2_| |_3_| |_4_| |_5_|");
-	printf("\nEm qual armário você acha que o goblin está [digite o número]: ");
+	printf("\nEm qual armÃ¡rio vocÃª acha que o goblin estÃ¡ [digite o nÃºmero]: ");
+	printf("\n|_1_| |_2_| |_3_| |_4_| |_5_|");
 	scanf("%d", &resposta);
 	
 	while (resposta != goblin){
 		
-		printf("Desculpe! O goblin ainda está à espreita em outro lugar!");
-		printf("|_1_| |_2_| |_3_| |_4_| |_5_|");
-		printf("\nEm qual armário você acha que o goblin está [digite o número]: ");
+		printf("\nDesculpe! O goblin ainda estÃ¡ Ã  espreita em outro lugar!");
+		printf("\nEm qual armÃ¡rio vocÃª acha que o goblin estÃ¡ [digite o nÃºmero]: ");
+		printf("\n|_1_| |_2_| |_3_| |_4_| |_5_|");
 		scanf("%d", &resposta);
 	}
-	printf("Bom trabalho %c!!! Você encontrou o goblin. Ele estava com tanto medo que fugiu!", nome);
+	printf("Bom trabalho %c!!! VocÃª encontrou o goblin. Ele estava com tanto medo que fugiu!", nome);
+	
+	printf("\nDeseja repetir o codigo? (s/n): ");
+    scanf(" %c", &continuar);  // Espaco antes de %c para ignorar espacos e quebras de linha
+	
+	} while (continuar == 's' || continuar == 'S');
 	
 	return 0;
 }
