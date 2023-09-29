00ba965ba179155e69c14e4faacaa0f2d72d3350
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-09-29 20:03:16 -0300

    Add files via upload

--- atividadesextras/atividade_2.cpp
+++ atividadesextras/atividade_2.cpp
@@ -1,51 +1,52 @@
 #include<stdio.h>
 #include<locale.h>
-#include<ctype.h> // biblioteca pro toupper
-#include<stdlib.h> // biblioteca pro numero aleatorio
-#include<time.h> // biblioteca faz com q o rand gere numeros aleatorios toda vaz q executado-
 
 int main(){
 	
 	setlocale(LC_ALL, "portuguese");
 	
-	char nome[21], opcao, continuar;// armazena um nome com 21 caracteres
-	int resposta, goblin;
+	int opcao, esfera, limpeza, troca, quebrado;
+	float total;
+	char continuar;
+	esfera = 0;
+	limpeza = 0;
+	troca = 0;
+	quebrado = 0;
+	total = 0;
 	
-	 do {
+	do{
 	
-	printf("digite seu nome: ");
-	scanf("%s", nome);// não vai e comercial pq é string
 	
-	printf("Olá %s !", nome);
-	 
-	printf("\nquer jogar? S/N");
-	scanf(" %c", &opcao);
-	
-	if(toupper(opcao)== 'N'){
-		printf("\nOK fica pra próxima!");
-		return 0;
-	}
-	
-	srand(time(NULL));
-	goblin = 1 + rand() % 5; // numero aleatorio entre 0 ate 4
-	
-	printf("\nEm qual armário você acha que o goblin está [digite o número]: ");
-	printf("\n|_1_| |_2_| |_3_| |_4_| |_5_|");
-	scanf("%d", &resposta);
-	
-	while (resposta != goblin){
+	printf("[1] Necessita da esfera.\n[2] Necessita de limpeza.\n[3] Necessita troca de cabo ou conector.\n[4] Quebrado ou inutilizado.");
+
+	printf("\nselecione o defeito: ");
+	scanf("%d", &opcao);
+	switch (opcao){
 		
-		printf("\nDesculpe! O goblin ainda está à espreita em outro lugar!");
-		printf("\nEm qual armário você acha que o goblin está [digite o número]: ");
-		printf("\n|_1_| |_2_| |_3_| |_4_| |_5_|");
-		scanf("%d", &resposta);
+		case 1 : esfera++;
+		break;
+		case 2 : limpeza++;
+		break;
+		case 3 : troca++; 
+		break;
+		case 4 : quebrado++; 
+		break;
 	}
-	printf("Bom trabalho %c!!! Você encontrou o goblin. Ele estava com tanto medo que fugiu!", nome);
 	
-	printf("\nDeseja repetir o codigo? (s/n): ");
-    scanf(" %c", &continuar);  // Espaco antes de %c para ignorar espacos e quebras de linha
+	
+	
+	printf("\ndeseja continuar? S/N");
+	scanf(" %c", &continuar);
 	
 	} while (continuar == 's' || continuar == 'S');
+	
+	total = esfera + limpeza + troca + quebrado;
+	
+	printf("\nquantidade de mouses: %d", total );
+	printf("\nNecessita da esfera: %d ( %.2f %%)", esfera, esfera * 100 / total);
+	printf("\nNecessita de limpeza: %d ( %.2f %%)", limpeza, limpeza * 100 / total);
+	printf("\nNecessita troca de cabo ou conector: %d (%.2f %%)", troca, troca * 100 / total);
+	printf("\nQuebrado ou inutilizado: %d ( %.2f %%)", quebrado, quebrado * 100 / total );
 	
 	return 0;
 }
