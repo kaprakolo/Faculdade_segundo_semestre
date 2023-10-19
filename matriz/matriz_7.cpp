690a375e3e4e40c0c031c9b83e441f2dca1d2334
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-10-18 21:20:56 -0300

    Add files via upload

---
+++ matriz/matriz_7.cpp
@@ -0,0 +1,19 @@
+//Escreva um programa que leia uma matriz M de 5 x 5 elementos e,
+// utilizando estruturas de repetição,apresente na tela a soma dos elementos da diagonal principal da matriz
+#include<stdio.h>
+int main(){
+	
+	int matriz [5][5] = {{1,2,3,4,5},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}}, i, j, soma;
+
+	soma = 0;
+	
+	for(i = 0; i < 5; i++){
+		soma = soma + matriz[i][i];
+	}
+	
+
+	
+	printf(" soma = %d ", soma);
+	
+	return 0; 
+}
