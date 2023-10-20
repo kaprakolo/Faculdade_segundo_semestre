b11d7a7efdddd99807e40371510a4f4e7ea073fb
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-10-20 16:56:58 -0300

    Add files via upload

---
+++ matriz/matriz_13.cpp
@@ -0,0 +1,49 @@
+//Escreva um programa que leia uma matriz M de 4 x 4 elementos e, 
+//utilizando estruturas de repetição,apresente na tela a soma dos elementos marcados com x
+#include<stdio.h>
+int main(){
+	
+	int matriz[4][5] = {{5,4,2,8,7},{3,6,9,1,4},{0,3,5,2,9},{4,9,7,6,2}}, i, j, vetor[4], menor;
+	
+	
+	for(i = 0; i < 4; i++){
+		
+		menor=1000;
+		for(j = 0; j < 5; j++){
+			
+			if(matriz[i][j] < menor){
+			
+				menor = matriz[i][j];
+			}
+		}
+		
+		printf("\n menor = %d", menor);
+		
+		vetor[i] = menor;
+		
+	}
+	printf("\n");
+	
+	printf("o menor valor de cada linha: ");
+			
+	for(i = 0;i < 4; i++){
+	
+		printf(" %d", vetor[i]);
+			
+	}
+	printf("\n");
+	
+	printf("da matriz: \n");
+	
+	for(i = 0; i < 4; i++){
+		
+		for(j = 0; j < 5; j++){
+			
+			printf(" %d", matriz[i][j]);
+		}
+		printf(" \n");
+	}
+	
+ 	
+	return 0;
+}
