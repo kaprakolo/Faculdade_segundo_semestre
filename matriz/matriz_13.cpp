2f885cbbc3edd622e1821f079df6f9a087e5ea62
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-10-20 19:20:03 -0300

    Update matriz_13.cpp

--- matriz/matriz_13.cpp
+++ matriz/matriz_13.cpp
@@ -3,24 +3,19 @@
 #include<stdio.h>
 int main(){
 	
-	int matriz[4][5] = {{5,4,2,8,7},{3,6,9,1,4},{0,3,5,2,9},{4,9,7,6,2}}, i, j, vetor[4], menor;
-	
+	int matriz[4][5] = {{5,4,2,8,7},{3,6,9,1,4},{0,3,5,2,9},{4,9,7,6,2}}, i, j, vetor[4];
 	
 	for(i = 0; i < 4; i++){
 		
-		menor=1000;
-		for(j = 0; j < 5; j++){
+		vetor[i] = matriz[i][j];
+		
+		for(j = 0; j < 4; j++){
 			
-			if(matriz[i][j] < menor){
+			if(matriz[i][j] <vetor[i]){
 			
-				menor = matriz[i][j];
+				vetor[i] = matriz[i][j];
 			}
 		}
-		
-		printf("\n menor = %d", menor);
-		
-		vetor[i] = menor;
-		
 	}
 	printf("\n");
 	
@@ -29,7 +24,6 @@ int main(){
 	for(i = 0;i < 4; i++){
 	
 		printf(" %d", vetor[i]);
-			
 	}
 	printf("\n");
 	
@@ -38,7 +32,6 @@ int main(){
 	for(i = 0; i < 4; i++){
 		
 		for(j = 0; j < 5; j++){
-			
 			printf(" %d", matriz[i][j]);
 		}
 		printf(" \n");
