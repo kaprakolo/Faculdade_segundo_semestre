fa2c18bb704b7138a1efdc91721c8ed3b8cbc4b5
Samuel <141376046+kaprakolo@users.noreply.github.com>
2023-10-26 20:00:58 -0300

    Add files via upload

---
+++ atividades_desenvolvimento_web/java/functio.js
@@ -0,0 +1,39 @@
+//const elementofinb = 10;
+
+
+//function fibonacci(elementofinb){
+//
+//let n1 = 0, n2 = 1;
+//let proximo = 0;
+//
+//    for(let iterador = 1; iterador < elementofinb; iterador++){
+//        //console.log(iterador + " --> " + n1);
+//        proximo = n1 + n2;
+//        n1 = n2;
+//        n2 = proximo;
+//    }
+//    console.log(iterador + " --> " + n1);
+//    return n1;
+//}
+
+let text = "eu adoro web basico";
+let i = 0;
+
+while (text[i] != undefined){
+    console.log(i + "é o caracter" + text[i]);
+    i++;
+}
+function calculartamanhodotexto(texto){
+    let i = 0;
+
+while (text[i] != undefined){
+    console.log(i + "é o caracter" + text[i]);
+    i++;
+}
+return i;
+}  
+
+
+
+
+return;
